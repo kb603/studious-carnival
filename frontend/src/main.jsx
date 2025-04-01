2aa7bbb959df43afa16d734fe51d3031d5d77c4a
kb603 <kojoaddobaffour@gmail.com>
2025-04-01 22:46:09 +0000

    Add Redux Toolkit API slice and integrate with users and notes features

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,17 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { store } from "./app/store.js";
+import { Provider } from "react-redux";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
