e56674b3d04d22ba7258242ebb28eab3e9181a27
kb603 <kojoaddobaffour@gmail.com>
2025-04-01 20:08:26 +0000

    modified cors

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://www.dandrepairshop.com",
   "https://dandrepairshop.com",
+  "http://localhost:5173",
+  "http://localhost:5500",
 ];
 
 module.exports = allowedOrigins;
