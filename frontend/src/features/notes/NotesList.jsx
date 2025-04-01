81c6b925bd2a64606893c909db30329261f3906e
kb603 <kojoaddobaffour@gmail.com>
2025-04-01 19:21:57 +0000

    Add initial frontend components and routing setup

---
+++ frontend/src/features/notes/NotesList.jsx
@@ -0,0 +1,7 @@
+export default function NoteList() {
+  return (
+    <div>
+      <h1>Note List</h1>
+    </div>
+  );
+}
---
+++ frontend/src/features/users/UsersList.jsx
@@ -0,0 +1,7 @@
+export default function UsersList() {
+  return (
+    <div>
+      <h1>Users List</h1>
+    </div>
+  );
+}
